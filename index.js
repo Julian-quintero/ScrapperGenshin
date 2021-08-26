7b66a08d164c1e547950db21653f98b6d5f257dc
julian <julian.quinteror1@gmail.com>
2021-08-25 23:38:11 -0500

    first

--- index.js
+++ index.js
@@ -15,6 +15,42 @@ const urls = [
 const app = express()
 const port = 3000
 
+const funt = async () => {
+    try {
+      const browser = await chromium.puppeteer.launch({
+        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      });
+  
+      let page = await browser.newPage();
+  
+      for (let index = 0; index < urls.length; index++) {
+        await page.goto(urls[index], { waitUntil: "networkidle2" });
+  
+        let elementsHendles = await page.evaluate(() =>
+          Array.from(document.querySelectorAll("ul > li > strong")).map(
+            (x) => x.textContent
+          )
+        );
+        
+        await browser.close();
+  
+        console.log(elementsHendles);
+      }
+    } catch (error) {
+      console.log("error ", error);
+    }
+  
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    })
+    res.end("hello")
+  
+  }
+
 
 app.get('/', (req, res) => {
     res.send('Hello, Vercel!')
@@ -22,42 +58,9 @@ app.get('/', (req, res) => {
   
   app.listen(port, () => {
     console.log(`Express app hosted on Vercel listening at port ${port}`)
+    funt()
   })
 
-module.exports = async (req, res) => {
-  try {
-    const browser = await chromium.puppeteer.launch({
-      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    });
 
-    let page = await browser.newPage();
-
-    for (let index = 0; index < urls.length; index++) {
-      await page.goto(urls[index], { waitUntil: "networkidle2" });
-
-      let elementsHendles = await page.evaluate(() =>
-        Array.from(document.querySelectorAll("ul > li > strong")).map(
-          (x) => x.textContent
-        )
-      );
-      
-      await browser.close();
-
-      console.log(elementsHendles);
-    }
-  } catch (error) {
-    console.log("error ", error);
-  }
-
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-  })
-  res.end("hello")
-
-}
 
 
