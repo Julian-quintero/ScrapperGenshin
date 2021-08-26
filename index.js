82a86a90783dd9355901333e44dd6291c36b6c93
julian <julian.quinteror1@gmail.com>
2021-08-26 06:28:31 -0500

    first

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 
-const chromium = require("chrome-aws-lambda");
+
 
 
 
 const express = require('express')
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // running on the Vercel platform.
+  chrome = require('chrome-aws-lambda');
+  puppeteer = require('puppeteer-core');
+} else {
+  // running locally.
+  puppeteer = require('puppeteer');
+}
 
 
 const urls = [
@@ -14,15 +25,18 @@ const urls = [
 const app = express()
 const port = 3000
 
+
+
 const funt = async () => {
-    try {
-        const browser = await chromium.puppeteer.launch({
-            args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath,
-            headless: true,
-            ignoreHTTPSErrors: true,
-          })
+    try {      
+
+      let browser = await puppeteer.launch({
+        args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      });
   
       let page = await browser.newPage();
   
