495eabdb540d8304a3b08934a9d79f363ff7b769
julian <julian.quinteror1@gmail.com>
2021-08-25 21:17:42 -0500

    first

---
+++ index.js
@@ -0,0 +1,42 @@
+const puppeteer = require("puppeteer");
+
+//https://www.vg247.com/genshin-impact-codes
+//https://www.pockettactics.com/genshin-impact/codes
+
+const urls = [
+  "https://www.pockettactics.com/genshin-impact/codes",
+  "https://www.vg247.com/genshin-impact-codes",
+];
+
+const fun = async () => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+
+    let page = await browser.newPage();
+
+    for (let index = 0; index < urls.length; index++) {
+      await page.goto(urls[index], { waitUntil: "networkidle2" });
+
+      let elementsHendles = await page.evaluate(() =>
+        Array.from(document.querySelectorAll("ul > li > strong")).map(
+          (x) => x.textContent
+        )
+      );
+
+    
+
+      console.log(elementsHendles);
+    }
+
+    await browser.close();
+  } catch (error) {
+    console.log("error ", error);
+    await browser.close();
+  }
+};
+
+fun();
